afda7a5881de6fa2b4e68ecaa2fabaca02f80937
milodevs <122129480+MiloDevs@users.noreply.github.com>
2025-12-11 15:28:01 +0300

    Revert "feat: create paste and view paste ui #4"

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import { sveltekitOG } from "@ethercorps/sveltekit-og/plugin";
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit(), sveltekitOG()],
+	plugins: [tailwindcss(), sveltekit()]
 });
