6d36aeb3cc8abd8fe2178c947ad33e65dd0655c0
Pdzly <github@rooki.xyz>
2025-12-15 21:59:40 +0100

    fix: log request headers and client info in custom IP address getter

--- backend/app/ratelimit.py
+++ backend/app/ratelimit.py
@@ -1,9 +1,10 @@
-
 from slowapi import Limiter
 from starlette.requests import Request
 
 
-def get_ip_address(request:Request):
+def get_ip_address(request: Request):
+    print(request.headers)
+    print(request.client)
     if request.headers.get("X-Forwarded-For", None):
         return request.headers.get("X-Forwarded-For")
     if not request.client or not request.client.host:
